5cf9b06e10eb24b69ea4b94d0eeadfcf0a4aa05a
Uwem Nelson <108585616+uwemNelson@users.noreply.github.com>
2022-07-14 17:54:15 +0000

    challenge -1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum=0;
+  for(let i=0; i<= arrayOfNumbers.length -1; i++)
+    {
+      sum += arrayOfNumbers[i];
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
